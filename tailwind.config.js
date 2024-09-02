1be69451841167d21970c766b63ab956e906960e
HASSNAIN ZIA <hassnainzia.dev121@gmail.com>
2024-09-03 02:09:34 +0500

    remove extra custom vanilla css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./*.html", "./src/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "linear-colors":
+          "linear-gradient(134.79deg, rgba(112, 59, 247, 0.5) -0.01%, rgba(112, 59, 247, 0) 9.65%)",
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        ".border-linear": {
+          border: "1px solid",
+          borderImage:
+            "linear-gradient(to bottom right, rgba(112, 59, 247, 1) 12.99%, rgba(38, 38, 38, 1) 39%) 1",
+          borderRadius: "inherit",
+          zIndex: "-1",
+        },
+      };
+
+      addUtilities(newUtilities, ["responsive", "hover"]);
+    },
+  ],
 };
