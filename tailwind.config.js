662dd89509e35cd8d39eaf24a5cdc6de83007a5a
hassnainzia121 <hassnainzia.dev121@gmail.com>
2024-09-03 17:32:14 +0500

    remove all vanilla css

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,31 @@ module.exports = {
   content: ["./*.html", "./src/*.html"],
   theme: {
     extend: {
+      fontFamily: {
+        urbanist: ["Urbanist", "sans-serif"],
+      },
       backgroundImage: {
         "linear-colors":
           "linear-gradient(134.79deg, rgba(112, 59, 247, 0.5) -0.01%, rgba(112, 59, 247, 0) 9.65%)",
+      },
+      backgroundImage: {
+        "linear-gradient":
+          "linear-gradient(95.93deg, #262626 -14.82%, rgba(38, 38, 38, 0) 40.46%)",
+      },
+      backgroundImage: {
+        "img-for-header": "url('./assets/homepage/abstract-design.png')",
+      },
+      backgroundColor: {
+        "header-bg": "rgba(26, 26, 26, 1)",
+      },
+      backgroundSize: {
+        cover: "cover",
+      },
+      backgroundPosition: {
+        center: "center",
+      },
+      backgroundRepeat: {
+        "no-repeat": "no-repeat",
       },
     },
   },
